e1e9e1c328f101af6fef7aa2b7c127fcf08f83f0
Greg <greg@hiro.so>
2025-03-06 00:44:41 -0500

    started a project

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import SplashPage from './components/SplashPage';
+import './App.css';
+
+function App() {
+  return <SplashPage />;
+}
+
+export default App;
---
+++ src/components/SplashPage.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+
+const SplashPage = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(1);
+  const TRANSITION_TIME = 1000; // 1 second in milliseconds
+  const TOTAL_IMAGES = 4;
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => 
+        prevIndex === TOTAL_IMAGES ? 1 : prevIndex + 1
+      );
+    }, TRANSITION_TIME);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div style={{
+      backgroundColor: 'black',
+      width: '100vw',
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <img 
+        src={`/images/logo-image-sequence/${currentImageIndex}.jpg`}
+        alt="Logo Animation"
+        style={{
+          height: '100vh',
+          width: '100vw',
+          objectFit: 'contain',
+          transition: `opacity ${TRANSITION_TIME}ms ease-in-out`,
+        }}
+      />
+    </div>
+  );
+};
+
+export default SplashPage; 
